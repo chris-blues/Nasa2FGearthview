dbe189612db8c2ddce59627700997946ff5c7e1b
chris_blues <chris@musicchris.de>
2017-01-10 21:55:24 +0100

    Add GPL header to convert.sh

--- convert.sh
+++ convert.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# convert.sh - a script to convert NASA sattelite images into textures for use with FGearthview (orbital rendering)
+# Copyright (C) 2016 chris_blues <chris@musicchris.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 VERSION="v0.11"
 
 # make sure the script halts on error
